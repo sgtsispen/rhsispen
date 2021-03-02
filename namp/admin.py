35c8adbd306ace842661211273f4a03402cc2f64
vinicius <stg@4tech.my.id>
2021-03-02 16:53:22 -0300

    Att07.1-correcao_enderecos

---
+++ namp/admin.py
@@ -0,0 +1,147 @@
+from django.contrib import admin
+
+# Register your models here.
+
+import json
+
+from django.contrib import admin
+from django.core.serializers.json import DjangoJSONEncoder
+
+
+from namp.models import Afastamento, ContatoEquipe, ContatoServ, EnderecoServ, EnderecoSetor, Equipe, Funcao, HistAfastamento, HistFuncao, HistLotacao, HistStatusFuncional, Jornada, Regiao, Servidor, Setor, StatusFuncional, TipoJornada
+
+#Cabeçalho da página
+admin.site.site_header = 'Administração do Núcleo de Apoio a Movimentação de Pessoal'
+
+# Registro de models
+class AfastamentoAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id_afastamento','__str__','descricao')
+admin.site.register(Afastamento, AfastamentoAdmin)
+
+class ContatoEquipeAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('__str__','contato')
+admin.site.register(ContatoEquipe, ContatoEquipeAdmin)
+	
+class ContatoServInline(admin.TabularInline):
+    model = ContatoServ
+    extra = 1
+
+class EnderecoServInline(admin.StackedInline):
+	model = EnderecoServ
+	extra = 1
+	max_num = 1
+
+#class EnderecoSetorAdmin(admin.ModelAdmin):
+#	pass
+#	list_display = ('endereco','numero','bairro','complemento','cep','municipio')
+#admin.site.register(EnderecoSetor, EnderecoSetorAdmin)
+
+class EnderecoSetorInline(admin.TabularInline):
+	model = EnderecoSetor
+	extra = 1
+
+class ServidorInline(admin.TabularInline):
+    model = Servidor
+	#readonly_fields = ('__str__', 'cpf', 'cargo')
+    fields = ('nome', str('id_matricula'),'vinculo','cpf', 'cargo') 
+    extra = 1
+
+class EquipeAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('nome','fk_setor','status','hora_inicial','categoria')
+	inlines = [ServidorInline]
+admin.site.register(Equipe, EquipeAdmin)
+
+class EquipeInline(admin.TabularInline):
+	model = Equipe
+	extra = 1
+
+class FuncaoAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id_funcao', 'nome')
+admin.site.register(Funcao, FuncaoAdmin)
+
+class HistAfastamentoAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id_hist_afastamento','data_inicial','duracao','fk_afastamento','fk_servidor')
+admin.site.register(HistAfastamento, HistAfastamentoAdmin)
+
+class HistFuncaoAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id_hist_funcao','data_inicio','data_final','fk_funcao','fk_servidor')
+admin.site.register(HistFuncao, HistFuncaoAdmin)
+
+class HistLotacaoAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('id_hist_lotacao','data_inicial','data_final','fk_servidor','fk_equipe')
+admin.site.register(HistLotacao, HistLotacaoAdmin)
+
+class HistStatusFuncionalAdmin(admin.ModelAdmin):
+	list_display = ('id_hist_funcional','data_inicial', 'data_final', 'fk_servidor', 'fk_status_funcional')
+	pass	
+admin.site.register(HistStatusFuncional, HistStatusFuncionalAdmin)
+
+class JornadaAdmin(admin.ModelAdmin):
+	pass
+	list_filter = ('assiduidade','fk_equipe','fk_tipo_jornada')	
+	list_display = ('data_jornada','fk_servidor', 'assiduidade','fk_equipe', 'fk_tipo_jornada')
+admin.site.register(Jornada, JornadaAdmin)
+
+class RegiaoAdmin(admin.ModelAdmin):
+	change_list_template = 'admin/namp/regiao/change_list.html'
+	list_display = ('id_regiao','nome')
+	
+	def changelist_view(self, request, extra_context=None):
+		#todas as instâncias de Setor do banco de dados
+
+		queryset = Regiao.objects.all()
+		regioes = ['Região ' + str(obj.id_regiao) for obj in queryset]
+		setores = [int(obj.setor_set.count()) for obj in queryset]
+
+		regioes_json = json.dumps(list(regioes), cls=DjangoJSONEncoder)
+		setores_json = json.dumps(list(setores), cls=DjangoJSONEncoder)
+
+		extra_context = extra_context or {"regioes": regioes_json, "setores": setores_json}
+
+		return super().changelist_view(request,extra_context=extra_context)
+admin.site.register(Regiao, RegiaoAdmin)
+
+class ServidorAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('nome','vinculo', 'cpf', 'sexo','cargo','tipo_vinculo','regime_juridico','situacao','fk_equipe')
+	list_filter = ('cargo','tipo_vinculo','regime_juridico','situacao',str('fk_equipe'))#,str('fk_endereco_serv'))
+	fieldsets = (
+		('DADOS PESSOAIS',{
+				'fields': ((str('id_matricula'),'vinculo'), ('nome', 'cpf'), 'sexo','dt_nasc')#, str('fk_endereco_serv'))
+		}),
+		('DADOS FUNCIONAIS',{
+				'fields': ('cargo','tipo_vinculo','regime_juridico','situacao', str('fk_equipe'))
+		}),
+	)
+	'''
+	Abaixo: apresentação dos forms da model ContatoServ dentro do form da model Servidor
+	'''	
+	inlines = [ContatoServInline, EnderecoServInline]
+admin.site.register(Servidor, ServidorAdmin)
+
+class SetorAdmin(admin.ModelAdmin):
+	pass
+	list_filter = ('fk_regiao','status','setor_sede')
+	list_display = ('id_setor', 'nome','fk_regiao','status', 'setor_sede')
+	search_fields = ('nome',)
+	inlines = [EquipeInline, EnderecoSetorInline]#add endecroSetor
+admin.site.register(Setor, SetorAdmin)
+
+class StatusFuncionalAdmin(admin.ModelAdmin):	
+	pass
+	list_display = (str('nome_status'),'descricao_status')
+admin.site.register(StatusFuncional, StatusFuncionalAdmin)
+
+class TipoJornadaAdmin(admin.ModelAdmin):
+	pass
+	list_display = ('carga_horaria', 'tipificacao', 'descricao')
+admin.site.register(TipoJornada, TipoJornadaAdmin)
+
+
---
+++ namp/models.py
@@ -0,0 +1,262 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+# Criação dos models
+'''CLASSES SEM CHAVE ESTRANGEIRA'''
+
+class Regiao(models.Model):
+	id_regiao = models.AutoField(primary_key=True)
+	nome = models.CharField(max_length=100)
+	def __str__(self):
+		return self.nome
+	class Meta:
+		verbose_name = "Região"
+		verbose_name_plural = "Regiões"
+		ordering = ["id_regiao"]
+
+class Funcao(models.Model):
+	id_funcao = models.CharField(primary_key=True, max_length=25) #Cod com a secad
+	nome = models.CharField(max_length=100)
+	def __str__(self):
+		return self.nome
+	class Meta:
+		verbose_name = "Função"
+		verbose_name_plural = "Funções"
+''' CLASSE QUE FUNCIONA
+class EnderecoSetor(models.Model):
+	id_endereco_setor = models.AutoField(primary_key=True)
+	uf = models.CharField(max_length=2, default='TO')
+	cep = models.CharField(max_length=8, blank=True, default='77000000')
+	municipio = models.CharField(max_length=100, default='Palmas')
+	endereco = models.CharField(max_length=100, default='Não registrado')
+	numero = models.CharField(max_length=10, blank=True)
+	bairro = models.CharField(max_length=100, blank=True)
+	complemento = models.CharField(max_length=100, blank=True)
+	def __str__(self):
+		return self.municipio + ' - ' + self.endereco
+	class Meta:
+		verbose_name = "Endereço do Setor"
+		verbose_name_plural = "Enderecos dos Setores"
+'''
+class Afastamento(models.Model):
+	id_afastamento = models.CharField('Código', primary_key=True, max_length=25) #Cod com a secad
+	tipificacao = models.CharField('Tipo de afastamento', max_length=50)
+	descricao = models.TextField(max_length=100)
+	def __str__(self):
+		return self.tipificacao
+	class Meta:
+		verbose_name = "Afastamento"
+		verbose_name_plural = "Afastamentos"
+
+class TipoJornada(models.Model):
+	id_tipo_jornada = models.AutoField(primary_key=True)
+	carga_horaria = models.PositiveIntegerField()
+	tipificacao = models.CharField(max_length=100)
+	descricao = models.CharField(max_length=100)
+	def __str__(self):
+		return self.tipificacao
+	class Meta:
+		verbose_name = "Tipo de Jornada"
+		verbose_name_plural = "Tipos de Jornadas"
+
+class StatusFuncional(models.Model):
+	id_status_funcional = models.AutoField(primary_key=True)
+	nome_status = models.CharField(max_length=50)
+	descricao_status = models.CharField(max_length=100)
+	def __str__(self):
+		return self.nome_status	
+	class Meta:
+		verbose_name = "Status Funcional"
+		verbose_name_plural = "Status Funcionais"
+
+''' CLASSES COM CHAVE ESTRANGEIRA'''
+
+'''OneToOneField =1..1
+   Foreignkey = 1..*
+   ManyToManyField = *..*
+   SET_DEFAULT = se apagado a referencia, vai pra outra tabela determinada
+'''
+
+class Setor(models.Model):
+	id_setor = models.CharField('Código', primary_key=True, max_length=50)
+	nome = models.CharField(max_length=100)
+	status = models.BooleanField('Setor Ativo', default=False)
+	setor_sede = models.BooleanField(default=False)
+	fk_regiao = models.ForeignKey(Regiao, on_delete = models.RESTRICT, verbose_name='Região', default=5) #RESTRICT: proibe a exclussão de região referenciada em setor
+	#CASCATE: se excluido o setor, será excluido o objeto referenciado(endereco_setor)
+	#ENREREÇO SETOR TEM FK PRA SETOR 
+	#fk_endereco_setor = models.OneToOneField(EnderecoSetor, on_delete = models.RESTRICT, verbose_name='Endereço')
+	def __str__(self):
+		return self.nome
+	class Meta:
+		verbose_name = "Setor"
+		verbose_name_plural = "Setores"
+
+class EnderecoSetor(models.Model):
+	id_endereco_setor = models.AutoField(primary_key=True)
+	uf = models.CharField(max_length=2, default='TO')
+	cep = models.CharField(max_length=8, blank=True, default='77000000')
+	municipio = models.CharField(max_length=100, default='Palmas')
+	endereco = models.CharField(max_length=100, default='Não registrado')
+	numero = models.CharField(max_length=10, blank=True)
+	bairro = models.CharField(max_length=100, blank=True)
+	complemento = models.CharField(max_length=100, blank=True)
+	fk_setor = models.ForeignKey(Setor, on_delete = models.RESTRICT, verbose_name='Setor')
+	def __str__(self):
+		return self.municipio + ' - ' + self.endereco
+	class Meta:
+		verbose_name = "Endereço do Setor"
+		verbose_name_plural = "Enderecos dos Setores"
+
+class Equipe(models.Model):
+	id_equipe = models.AutoField(primary_key=True)
+	nome = models.CharField(max_length=50)
+	status = models.BooleanField(default=False)
+	hora_inicial = models.TimeField(auto_now= False, auto_now_add=False)
+	categoria = models.CharField(max_length=50)
+	fk_setor = models.ForeignKey(Setor, on_delete = models.RESTRICT, verbose_name='Setor')
+	def __str__(self):
+		return self.nome + ' - ' + (self.fk_setor.nome)
+	class Meta:
+		verbose_name = "Equipe"
+		verbose_name_plural = "Equipes"
+
+class ContatoEquipe(models.Model):
+	CELULAR = 'Telefone Celular'
+	FIXO = 'Telefone Fixo'
+	EMAIL = 'E-mail'
+	CONTATOS_CHOICES = (
+	    (CELULAR,'Telefone Celular'), #validators=[RegexValidator(r'^(\d{2})\d{5}-\d{4}$')],
+	    (FIXO,'Telefone Fixo'),
+	    (EMAIL,'E-mail'), #EmailValidator(message = "Digite o E-mail", code = "E-mail inválido", whitelist=None), #EmailValidator(message = "Digite o E-mail", code = "E-mail inválido", whitelist=None)
+	)
+	id_contato_equipe = models.AutoField(primary_key=True)
+	tipificacao = models.CharField('Tipo de contato', max_length=50, choices=CONTATOS_CHOICES)
+	contato = models.CharField(max_length=50)
+	fk_equipe = models.ForeignKey(Equipe, on_delete = models.RESTRICT, verbose_name='Equipe')
+	def __str__(self):
+		return str(self.fk_equipe)
+	class Meta:
+		verbose_name = "Contato da Equipe"
+		verbose_name_plural = "Contato das Equipes"
+
+class Servidor(models.Model):
+	Masculino = 'M'
+	Feminino = 'F'
+	GEN_CHOICES = (
+		(Masculino, 'M'),
+		(Feminino, 'F'),
+		)
+	id_matricula = models.CharField('Matrícula', primary_key=True, max_length=30)
+	vinculo = models.CharField('Vínculo',max_length=3) #Parte da matricula
+	nome = models.CharField(max_length=100)
+	cpf = models.CharField('CPF',max_length=12)
+	sexo = models.CharField('sexo\n(Feminino/Masculino)', max_length=1, choices=GEN_CHOICES)
+	dt_nasc = models.DateField('Data de Nascimento')
+	cargo = models.CharField(max_length=50)
+	tipo_vinculo = models.CharField('Tipo de Vínculo',max_length=50)
+	regime_juridico = models.CharField('Regime Jurídico',max_length=50)
+	situacao = models.BooleanField(default=False)
+	fk_equipe = models.ForeignKey(Equipe, on_delete = models.RESTRICT, verbose_name='Equipe')
+	#def __str__(self):
+	#	return self.nome
+	class Meta:
+		verbose_name = "Servidor"
+		verbose_name_plural = "Servidores"
+
+class EnderecoServ(models.Model):
+	id_endereco_serv = models.AutoField(primary_key=True)
+	uf = models.CharField(max_length=2) 
+	cep = models.CharField(max_length=8)
+	municipio = models.CharField(max_length=100)
+	endereco = models.CharField(max_length=100)
+	numero = models.CharField(max_length=10)
+	bairro = models.CharField(max_length=100)
+	complemento = models.CharField(max_length=100, blank=True)
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name = 'Servidor')
+	#def __str__(self):
+		#return ''
+	class Meta:
+		verbose_name = "Endereço do Servidor"
+		verbose_name_plural = "Endereço do Servidor"
+
+class HistFuncao(models.Model):
+	id_hist_funcao = models.AutoField(primary_key=True)
+	data_inicio = models.DateField()
+	data_final = models.DateField()
+	fk_funcao = models.ForeignKey(Funcao, on_delete = models.RESTRICT, verbose_name='Função')
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	def __str__(self):
+		return str(self.id_hist_funcao)
+	class Meta:
+		verbose_name = "Histórico de Função"
+		verbose_name_plural = "Históricos de Funcões"
+
+class ContatoServ(models.Model):
+	CELULAR = 'Telefone Celular'
+	FIXO = 'Telefone Fixo'
+	EMAIL = 'E-mail'
+	CONTATOS_CHOICES = (
+	    (CELULAR,'Telefone Celular'),
+	    (FIXO,'Telefone Fixo'),
+	    (EMAIL,'E-mail'),
+	)
+	id_contato_serv = models.AutoField(primary_key=True)
+	tipo_contato = models.CharField('Tipo de contato', max_length=100,choices=CONTATOS_CHOICES)
+	contato = models.CharField(max_length=100)
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	def __str__(self):
+		return ''
+	
+	class Meta:
+		verbose_name = "Contato do Servidor"
+		verbose_name_plural = "Contatos do Servidor"
+
+class HistAfastamento(models.Model):
+	id_hist_afastamento = models.AutoField(primary_key=True)
+	data_inicial = models.DateField()
+	duracao = models.PositiveIntegerField()
+	fk_afastamento = models.ForeignKey(Afastamento, on_delete = models.RESTRICT, verbose_name='Afastamento')
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	def __str__(self):
+		return str(self.id_hist_afastamento)
+	class Meta:
+		verbose_name = "Histório de Afastamento"
+		verbose_name_plural = "Históricos de Afastamentos"
+
+class HistLotacao(models.Model):
+	id_hist_lotacao = models.AutoField(primary_key=True)
+	data_inicial =	models.DateField()
+	data_final = models.DateField()
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	fk_equipe = models.ForeignKey(Equipe, on_delete = models.RESTRICT, verbose_name='Equipe')
+	def __str__(self):
+		return str(self.id_hist_lotacao)
+	class Meta:
+		verbose_name = "Histórico de Lotação"
+		verbose_name_plural = "Históricos de Lotações"
+
+class HistStatusFuncional(models.Model):
+	id_hist_funcional = models.AutoField(primary_key=True)
+	data_inicial = models.DateField()
+	data_final = models.DateField()
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	fk_status_funcional = models.ForeignKey(StatusFuncional, on_delete = models.RESTRICT, verbose_name='Status Funcional')
+	def __str__(self):
+		return str(self.id_hist_funcional)
+	class Meta:
+		verbose_name = "Histórico de Status Funcional"
+		verbose_name_plural = "Histórico de Status Funcionais"
+
+class Jornada(models.Model):
+	id_jornada = models.AutoField(primary_key=True)
+	data_jornada = models.DateField()
+	assiduidade = models.BooleanField(default=False)
+	fk_servidor = models.ForeignKey(Servidor, on_delete = models.RESTRICT, verbose_name='Servidor')
+	fk_equipe = models.ForeignKey(Equipe, on_delete = models.RESTRICT, verbose_name='Equipe')
+	fk_tipo_jornada = models.ForeignKey(TipoJornada, on_delete = models.RESTRICT, verbose_name='Tipo Jornada')
+	def __str__(self):
+		return str(self.id_jornada) 
+	class Meta:
+		verbose_name = "Jornada"
+		verbose_name_plural = "Jornadas"
